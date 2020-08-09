3fa6a5213b42f747003fea94a7208c596105df45
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-08-09 22:35:51 +0300

    #14 Categories (create)

---
+++ src/components/CategoryCreate.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="col s12 m6">
+    <div>
+      <div class="page-subtitle">
+        <h4>Создать</h4>
+      </div>
+
+      <form @submit.prevent="submitHandler">
+        <div class="input-field">
+          <input
+              id="name"
+              type="text"
+              v-model="title"
+              :class="{ invalid: $v.title.$dirty && !$v.title.required }"
+          >
+          <label for="name">Название</label>
+          <span
+            v-if="$v.title.$dirty && !$v.title.required"
+            class="helper-text invalid"
+          >Введите название категории</span>
+        </div>
+
+        <div class="input-field">
+          <input
+              id="limit"
+              type="number"
+              v-model.number="limit"
+              :class="{ invalid: $v.limit.$dirty && !$v.limit.minValue }"
+          >
+          <label for="limit">Лимит</label>
+          <span
+            class="helper-text invalid"
+            v-if="$v.limit.$dirty && !$v.limit.minValue"
+          >Минимальная величина {{ $v.limit.$params.minValue.min }}</span>
+        </div>
+
+        <button class="btn waves-effect waves-light" type="submit">
+          Создать
+          <i class="material-icons right">send</i>
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { minValue, required } from 'vuelidate/lib/validators'
+import M from 'materialize-css'
+export default {
+  data () {
+    return {
+      title: '',
+      limit: 100
+    }
+  },
+  validations: {
+    title: { required },
+    limit: { minValue: minValue(100) }
+  },
+  mounted () {
+    M.updateTextFields()
+  },
+  methods: {
+    async submitHandler () {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+        // eslint-disable-next-line no-useless-return
+        return
+      }
+      try {
+        const category = await this.$store.dispatch('createCategory', {
+          title: this.title,
+          limit: this.limit
+        })
+        this.title = ''
+        this.limit = 100
+        this.$v.$reset()
+        this.$message('Категория успешно создана!')
+        this.$emit('created', category)
+      } catch (e) {}
+    }
+  }
+}
+</script>
