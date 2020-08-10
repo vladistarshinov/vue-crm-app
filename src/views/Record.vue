7588718966895c63d58112576e23a5f4382ea196
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-08-10 18:43:20 +0300

    16 Record

--- src/views/Record.vue
+++ src/views/Record.vue
@@ -4,11 +4,16 @@
       <h3>Новая запись</h3>
     </div>
 
-    <form class="form">
+    <Loader v-if="loading" />
+    <p class="center" v-else-if="!categories.length">Категорий пока нет. <router-link to="/categories">Добавить новую категорию</router-link></p>
+    <form class="form" v-else @submit.prevent="submitHandler">
       <div class="input-field" >
-        <select>
+        <select ref="select" v-model="category">
           <option
-          >name cat</option>
+            v-for="category of categories"
+            :key="category.id"
+            :value="category.id"
+          >{{category.title}}</option>
         </select>
         <label>Выберите категорию</label>
       </div>
@@ -20,6 +25,7 @@
               name="type"
               type="radio"
               value="income"
+              v-model="type"
           />
           <span>Доход</span>
         </label>
@@ -32,6 +38,7 @@
               name="type"
               type="radio"
               value="outcome"
+              v-model="type"
           />
           <span>Расход</span>
         </label>
@@ -41,19 +48,28 @@
         <input
             id="amount"
             type="number"
+            v-model.number="amount"
+            :class="{ invalid: $v.amount.$dirty && !$v.amount.minValue }"
         >
         <label for="amount">Сумма</label>
-        <span class="helper-text invalid">amount пароль</span>
+         <span
+            class="helper-text invalid"
+            v-if="$v.amount.$dirty && !$v.amount.minValue"
+          >Минимальное значение - {{ $v.amount.$params.minValue.min }}</span>
       </div>
 
       <div class="input-field">
         <input
             id="description"
             type="text"
+            v-model="description"
+            :class="{ invalid: $v.description.$dirty && !$v.description.required }"
         >
         <label for="description">Описание</label>
-        <span
-              class="helper-text invalid">description пароль</span>
+         <span
+            v-if="$v.description.$dirty && !$v.description.required"
+            class="helper-text invalid"
+          >Введите название</span>
       </div>
 
       <button class="btn waves-effect waves-light" type="submit">
@@ -63,3 +79,84 @@
     </form>
   </div>
 </template>
+
+<script>
+import { minValue, required } from 'vuelidate/lib/validators'
+import { mapGetters } from 'vuex'
+import M from 'materialize-css'
+export default {
+  name: 'record',
+  data () {
+    return {
+      loading: true,
+      categories: [],
+      select: null,
+      category: null,
+      type: 'outcome',
+      amount: 10,
+      description: ''
+    }
+  },
+  validations: {
+    description: { required },
+    amount: { minValue: minValue(10) }
+  },
+  computed: {
+    ...mapGetters(['info']),
+    availabilityMoney () {
+      if (this.type === 'income') {
+        return true
+      }
+      return this.info.bill >= this.amount
+    }
+  },
+  methods: {
+    async submitHandler () {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
+
+      if (this.availabilityMoney) {
+        try {
+          await this.$store.dispatch('createRecord', {
+            categoryId: this.category,
+            amount: this.amount,
+            description: this.description,
+            type: this.type,
+            date: new Date().toJSON()
+          })
+
+          const bill = this.type === 'income'
+            ? this.info.bill + this.amount
+            : this.info.bill - this.amount
+          console.log(bill)
+          await this.$store.dispatch('updateInfo', { bill })
+          this.$message('Запись успешно создана')
+          this.$v.$reset()
+          this.amount = 10
+          this.description = ''
+        } catch (e) {}
+      } else {
+        this.$message(`Недостаточно средств на счете (не хватает ${this.amount - this.info.bill} руб.)`)
+      }
+    }
+  },
+  async mounted () {
+    this.categories = await this.$store.dispatch('fetchCategories')
+    this.loading = false
+    if (this.categories.length) {
+      this.category = this.categories[0].id
+    }
+    setTimeout(() => {
+      this.select = M.FormSelect.init(this.$refs.select)
+      M.updateTextFields()
+    }, 0)
+  },
+  destroyed () {
+    if (this.select && this.select.destroy) {
+      this.select.destroy()
+    }
+  }
+}
+</script>
